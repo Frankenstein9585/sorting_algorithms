d9db320faeaab62cfd23be59d47ef6884591b2e0
Donald Peters <Donny9585@gmail.com>
2023-02-24 15:23:30 +0000

    modified task 2 again

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap - swaps elements in an array
+ * @x: array element
+ * @y: array element
+ */
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
   * selection_sort - implements the selection sort algorithm
   *
   * @array: Array to be sorted
